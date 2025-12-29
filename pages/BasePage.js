99265bc494b74569d71673a359cd19f451cb56ca
Daria Olsson <dolssontest@gmail.com>
2025-12-29 12:20:13 -0500

    Add: Start and Refund dates

--- pages/BasePage.js
+++ pages/BasePage.js
@@ -11,7 +11,7 @@ export class BasePage {
   }
   
   async login(){
-    const code = Buffer.from(`${process.env.SEP_USERNAME}:${process.env.SEP_PASSWORD}`).toString("base64");
+    const code = Buffer.from(`${process.env.SEP_QA_USERNAME}:${process.env.SEP_QA_PASSWORD}`).toString("base64");
     await this.page.setExtraHTTPHeaders({Authorization: `Basic ${code}`,});
     await this.page.goto(process.env.SEP_QA_URL);
     BrowserUtility.verify_title(this.page, 'Checkout | Cydeo');

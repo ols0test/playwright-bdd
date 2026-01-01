61d09c26fae0f2a0c25a885715f0e1ca235f082e
Daria Olsson <dolssontest@gmail.com>
2026-01-01 11:21:03 -0500

    Add 6scenarios to sep09 feature

--- features/getting-started/DisplayProductInfo.feature
+++ features/getting-started/DisplayProductInfo.feature
@@ -15,3 +15,28 @@ Feature: Display the product information
 
     Background:
         Given user is on the enrollment page
+
+    @sep09-1
+    Scenario: The product name should be displayed on the information card.
+        Then the product name should be displayed on the information card
+
+    @sep09-2
+    Scenario: The product name on the information card matches the product name on the left side of the screen.
+        Then the start application stepper circle color should be blue
+
+    @sep09-3
+    Scenario: The price of the product should be displayed.
+        Then the price of the product should be displayed
+
+    @sep09-4
+    Scenario: The text indicating a flexible payment plan should be available and displayed.
+        Then the text indicating a flexible payment plan should be displayed
+
+    @sep09-5
+    Scenario: The program start date should be displayed.
+        Then the program start date is displayed
+
+    @sep09-6
+    Scenario: The return policy and the final date for returns should be displayed.
+        Then the return policy should be displayed
+        And the program refund date is displayed
--- steps/getting-started/displayProductInfo.js
+++ steps/getting-started/displayProductInfo.js
@@ -2,3 +2,28 @@ import { Given, Then, When } from "@cucumber/cucumber";
 import { expect} from "@playwright/test";
 import { startApplicationPage, page } from "../../globalPagesSetup.js";
 import { productInfo } from "../../utilities/qa-data-reader.js";
+
+Then("the product name should be displayed on the information card", async function () {
+    const ACTUAL_PROGRAM_NAME_ON_INFO_CARD = await startApplicationPage.programNameOnInfoCard.innerText();
+    const EXPECTED_PROGRAM_NAME_ON_INFO_CARD = "Test Automation with Selenium";
+    expect(ACTUAL_PROGRAM_NAME_ON_INFO_CARD).toBe(EXPECTED_PROGRAM_NAME_ON_INFO_CARD);
+});
+
+Then("the price of the product should be displayed", async function () {
+    await expect(startApplicationPage.programBasePrice).toHaveText("$500");
+    await expect(startApplicationPage.discountedPrice).toHaveText("$400");
+});
+
+Then("the text indicating a flexible payment plan should be displayed", async function () {
+    const ACTUAL_TEXT_ABOUT_FLEXIBLE_PLAN = await startApplicationPage.flexiblePaymentsPlanAvailableText.innerText();
+    const EXPECTED_TEXT_ABOUT_FLEXIBLE_PLAN = "Flexible payments plan available";
+    expect(ACTUAL_TEXT_ABOUT_FLEXIBLE_PLAN).toBe(EXPECTED_TEXT_ABOUT_FLEXIBLE_PLAN);
+    expect(await startApplicationPage.flexiblePaymentsPlanAvailableText.isVisible()).toBe(true);
+});
+
+Then("the return policy should be displayed", async function () {
+    const ACTUAL_RETURN_POLICY_TEXT = await startApplicationPage.refundPolicytext.innerText();
+    const EXPECTED_RETURN_POLICY_TEXT = "100% refund policy until\nMay 10, 2025";
+    expect(ACTUAL_RETURN_POLICY_TEXT).toBe(EXPECTED_RETURN_POLICY_TEXT);
+    expect(await startApplicationPage.refundPolicytext.isVisible()).toBe(true);
+});
